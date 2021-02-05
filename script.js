0b8bfde9e86c9264738af465ade651be361e1cc5
David Sozashvili <datosoza.js@gmail.com>
2021-02-05 22:54:54 +0400

    back kill

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 
 const darks = document.querySelectorAll('.dark');
-
 const tds = document.querySelectorAll('td');
-
 
 tds.forEach((element, index) => {
     if(!element.dataset.id) {
         element.dataset.id = index;
     }
 });
-
-
 
 darks.forEach(element => {
     element.addEventListener('dragstart', e => {
@@ -22,53 +18,52 @@ darks.forEach(element => {
     });
 });
 
-
 darks.forEach(element => {
-    
-        element.addEventListener("drop", e => {
-            if (e.target.tagName !== 'TD' || e.target.firstElement) {
-                return;
-            }
-            let id = e.dataTransfer.getData('id');
-            let takenDrought = document.getElementById(id);
-            
-            if(takenDrought.classList.contains('red') && +e.target.dataset.id < +takenDrought.parentElement.dataset.id) {
-                takenDrought.removeAttribute('id');
-                return;
-            }
-            
-            if(takenDrought.classList.contains('black') && +e.target.dataset.id > +takenDrought.parentElement.dataset.id) {
-                takenDrought.removeAttribute('id');
-                return;
-            }
-            
-            let difference;
-            function differenceCount () {
-                difference =  Math.abs(e.target.dataset.id - takenDrought.parentElement.dataset.id);
-            }
-            differenceCount();
+    element.addEventListener("drop", e => {
+        if (e.target.tagName !== 'TD' || e.target.firstElement) {
+            return;
+        }
+        let id = e.dataTransfer.getData('id');
+        let takenDraughts = document.getElementById(id);
 
-            takenDrought.removeAttribute('id');
+        let difference;
+        function differenceCount () {
+            difference = Math.abs(e.target.dataset.id - takenDraughts.parentElement.dataset.id);
+        }
+        differenceCount();
 
-            if(difference == 7 || difference == 9) {
-                e.target.appendChild(takenDrought);
-                return;
-            }
-            if(difference == 14 || difference == 18) {
-                let g = (+takenDrought.parentElement.dataset.id + +e.target.dataset.id)/2;
-                tds.forEach(element => {
-                    if(element.dataset.id == g && element.firstElementChild && 
-                        !element.firstElementChild.classList.contains(takenDrought.classList[0])) {
-                        console.log(element.firstElementChild);
+        if(difference == 14 || difference == 18) {
+            let avarage = (+takenDraughts.parentElement.dataset.id + +e.target.dataset.id)/2;
+            tds.forEach(element => {
+                if(element.dataset.id == avarage && element.firstElementChild && 
+                    !element.firstElementChild.classList.contains(takenDraughts.classList[0])) {
+                    setTimeout(() => {
                         element.firstElementChild.remove();
-                    } else {
-                        return;
-                    }
-                    e.target.appendChild(takenDrought);
-                });
-                
-            }
-        });
+                    }, 100);
+                } else {
+                    return;
+                }
+                e.target.appendChild(takenDraughts);
+            });   
+        }
+        
+        if(takenDraughts.classList.contains('red') && +e.target.dataset.id < +takenDraughts.parentElement.dataset.id) {
+            takenDraughts.removeAttribute('id');
+            return;
+        }
+        
+        if(takenDraughts.classList.contains('black') && +e.target.dataset.id > +takenDraughts.parentElement.dataset.id) {
+            takenDraughts.removeAttribute('id');
+            return;
+        }
+        
+        takenDraughts.removeAttribute('id');
+
+        if(difference == 7 || difference == 9) {
+            e.target.appendChild(takenDraughts);
+            return;
+        }
+    });
 });
 
 
